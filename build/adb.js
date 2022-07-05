08e54bd03607f7a1d5a7c914607e80f09b5d650c
linhy29 <linhy29@midea.com>
2022-07-05 14:54:58 +0800

    feat(projects): adb推送脚本支持非标A0设备

--- build/adb.js
+++ build/adb.js
@@ -1,11 +1,34 @@
 const env = require('dotenv').config().parsed
 const cp = require('child_process')
-console.log('push to ' + `/storage/emulated/0/Android/data/com.midea.ai.appliances/files/MideaHome/${env.DEVICE_INFO}/`)
-cp.exec(
-  `adb push "dist/${env.DEVICE_INFO}/." "/storage/emulated/0/Android/data/com.midea.ai.appliances/files/MideaHome/${env.DEVICE_INFO}/"`,
-  (err) => {
-    if (err) {
-      console.log('adb推送失败：', err)
+
+if (env.NON_STANDARD_DEVICE === 'false') {
+  // 标准设备
+  console.log(
+    'push to ' +
+      `/storage/emulated/0/Android/data/com.midea.ai.appliances/files/MideaHome/${env.DEVICE_INFO}/`
+  )
+  cp.exec(
+    `adb push "dist/${env.DEVICE_INFO}/." "/storage/emulated/0/Android/data/com.midea.ai.appliances/files/MideaHome/${env.DEVICE_INFO}/"`,
+    err => {
+      if (err) {
+        console.log('adb推送失败：', err)
+      }
     }
+  )
+} else {
+  // 使用A0的非标设备
+  for (const a0 of env.A0_LIST.split(',')) {
+    console.log(
+      'push to ' +
+        `/storage/emulated/0/Android/data/com.midea.ai.appliances/files/MideaHome/${env.PLUGIN_TYPE}_${a0}/`
+    )
+    cp.exec(
+      `adb push "dist/." "/storage/emulated/0/Android/data/com.midea.ai.appliances/files/MideaHome/${env.PLUGIN_TYPE}_${a0}/"`,
+      err => {
+        if (err) {
+          console.log('adb推送失败：', err)
+        }
+      }
+    )
   }
-)
+}
